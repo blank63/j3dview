11f8c7ae11f17ef653253a282d2042ba3e32335d
blank63 <cheerful.sisyphus@gmail.com>
2016-07-09 09:01:40 +0200

    Updated setup.py and README.md to reflect changes

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# j3dview
-View BMD/BDL files
+# J3D View
+J3D View is a GUI application for viewing and editing Nintendo GameCube/Wii BMD/BDL files.
+
+# Build
+**Note:** This section is only relevant if you intend to build the application or run the application from source.
+
+J3D View requires Python 3 with NumPy, PyOpenGL and PyQt5. Building extension modules requires Cython. Building an executable requires PyInstaller.
+
+Building extension modules requires Cython and a C compiler compatible with your Python installation. For Windows see https://matthew-brett.github.io/pydagogue/python_msvc.html to find a compatible compiler. To build the extension modules run the command:
+```bash
+$ setup.py build_ext --inplace
+```
+Building an executable requires PyInstaller. To build an executable run the command:
+```bash
+$ pyinstaller j3dview.spec
+```
+
+This has been tested with [CPython 3.5.1 32-bit](https://www.python.org/ftp/python/3.5.1/python-3.5.1.exe), [Visual Studio Community 2015](https://www.visualstudio.com/products/visual-studio-community-vs) (do a custom install and enable C++ support), NumPy 1.11.0, PyOpenGL 3.1.0, PyQt5 5.6, Cython 0.24 and PyInstaller 3.2 (note that there is a [bug](https://github.com/pyinstaller/pyinstaller/pull/1981) int this version) on Windows 8.1 64-bit. 
+
--- setup.py
+++ setup.py
@@ -1,39 +1,25 @@
 #!/usr/bin/env python3
 
-try:
-    from cx_Freeze import setup,Executable
-    has_cx_freeze = True
-except ImportError:
-    from distutils.core import setup
-    has_cx_freeze = False
-    print('Could not import cx_Freeze. Building executable not possible.')
-
-import platform
+from setuptools import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
 
-arguments = dict(
+texture = Extension(
+        'gx.texture',
+        ['gx/texture.pyx'],
+        include_dirs=[numpy.get_include()])
+
+setup(
         name='J3D View',
         version='0.4',
-        description='Nintendo GameCube/Wii BMD/BDL file viewer',
+        description='Nintendo GameCube/Wii BMD/BDL file viewer and editor',
+        url="https://github.com/blank63/j3dview/",
+        license="MIT",
         scripts = ['j3dview.py'],
-        py_modules=['gl','viewer_widget','explorer_widget','forms'],
-        packages=['btypes','gx','j3d'])
-
-arguments['ext_modules'] = cythonize(Extension(
-    'gx.texture',
-    ['gx/texture.pyx'],
-    include_dirs=[numpy.get_include()]))
-
-if has_cx_freeze:
-    base = 'Win32GUI' if platform.system() == 'Windows' else None
-    build_exe = dict(
-            includes=['viewer_widget','explorer_widget','forms'],
-            packages=['OpenGL.platform','OpenGL.arrays'],
-            include_files=[('ui/Editor.ui','ui/Editor.ui'),('ui/ViewSettingsForm.ui','ui/ViewSettingsForm.ui'),('ui/TextureForm.ui','ui/TextureForm.ui')])
-    arguments['executables'] = [Executable('j3dview.py',base=base)]
-    arguments['options'] = dict(build_exe=build_exe)
-
-setup(**arguments)
+        py_modules=['gl','qt'],
+        packages=['btypes','gx','j3d','widgets'],
+        package_data={'widgets':['*.ui']},
+        ext_modules=cythonize(texture),
+        install_requires=['numpy','PyOpenGL','PyQt5'])
 
